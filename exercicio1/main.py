52d812ccb9307832d39b7f241b2832f89f06ce36
Dante Pedrosa <58957540+dantepedrosa@users.noreply.github.com>
2026-04-14 23:04:26 +0000

    comit temporário

---
+++ exercicio1/main.py
@@ -0,0 +1,8 @@
+# 1
+
+bolo1 = Bolo(“Cenoura”, “Brigadeiro”, “Cenoura”, True)
+bolo2 = Bolo(“Formigueiro”, “Brigadeiro”, “Baunilha”, False)
+bolo3 = Bolo(“Formigueiro”, “Brigadeiro”, “Baunilha”, False)
+
+# 2
+
